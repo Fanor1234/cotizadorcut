b02a8e573440874ebad1930226ef45bd334513d7
Fanor1234 <luis_faurp@hotmail.com>
2023-05-26 10:59:42 -0400

    key

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from prompt import get_completion
 import openai
 import os
 
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
+# from dotenv import load_dotenv, find_dotenv
+# _ = load_dotenv(find_dotenv())
+# openai.api_key  = os.getenv('OPENAI_API_KEY')
 
-openai.api_key  = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 
 app = Flask(__name__)
